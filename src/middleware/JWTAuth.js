bd4fd6f30a469d0ed61dcbf75708b6d4f7639005
Chad Drake <chaddrake1313@yahoo.com>
2020-10-16 10:37:48 -0500

    cleaned code

--- src/middleware/JWTAuth.js
+++ src/middleware/JWTAuth.js
@@ -17,7 +17,6 @@ function requireAuth(req, res, next) {
       .getUserwithUsername(req.app.get('db'), payload.sub)
       .then((user) => {
         if (!user) {
-          console.log('no user found');
           return res.status(401).json({ error: 'Unauthorized request' });
         }
         req.user = user;
